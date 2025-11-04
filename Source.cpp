c2fcab023efac4963616d2d34afb7c574924eced
Nguyen Viet Toan <nguyentoan2004v@gmail.com>
2025-11-04 09:01:17 +0700

    chinhphuong

--- Source.cpp
+++ Source.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
-
+#include <stdio.h>
 using namespace std;
 int tong(int a, int b);
 int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(int a, int b);
-bool ktcp(int a, int b);
+void ktcp(int a){
+	
+	int n=a;
+	int i=0;
+	while (i*i<=n){
+		if(i*i==n){
+		cout<<"la so chinh phuong";
+		i++;
+		}
+		else{
+		cout<<"khong la so chinh phuong";
+		}
+	}
+}
 int main()
 {
 	int a =2,b=3;
@@ -14,6 +27,7 @@ int main()
 	cout<<"A";
 	cout<<"Tich ="<<a*b;
 	cout<<"B";
+	ktcp(a);
 	system("pause");
 	return 0;
 }
