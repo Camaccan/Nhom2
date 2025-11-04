9609c140423c54edd83924303473ba3da505d2b0
NguyenSyKhiem <khiemprobo135@gmail.com>
2025-11-04 08:59:51 +0700

    nt

--- Source.cpp
+++ Source.cpp
@@ -1,16 +1,25 @@
 #include <iostream>
+#inclue <math.h> 
 
 using namespace std;
 int tong(int a, int b);
 int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(int a, int b);
-bool ktsnt(int n);
+bool ktsnt(int n){
+    if (n<2) return false;
+    for (int i =2; i<= sqrt (n) ;i++)
+        if(n%i==0)
+            return false;
+    return true;
+}
 int main()
 {
 	int a =2,b=3;
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
 	cout<<"Tong ="<<a + b;
+	cout<<"So nguyen to" ;
+	ktsnt(2);
 	cout<<"A";
 	cout<<"Tich ="<<a*b;
 	cout<<"B";
